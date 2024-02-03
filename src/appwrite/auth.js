56646ba105715c6fdb17f54ef7eb1940b757c312
Kush Sharma <kush.73888@gmail.com>
2024-02-03 18:21:49 +0530

    Initial Commit - Auth JS

---
+++ src/appwrite/auth.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-useless-catch */
+import config from '../config/config'
+import { Client, Account, ID } from 'appwrite'
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({email, password});
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            console.log(`createAccount error: ${error}`);
+        }
+    }
+    async login({email, password}) {
+        try {
+          return  await this.account.createEmailSession(email, password);
+        } catch (error) {
+            console.log(`login error: ${error}`);
+        }
+    }
+
+    async getCurrentUser() {
+       try {
+       return await this.account.get();
+       } catch (error) {
+        console.log(`getCurrentUser error: ${error}`);
+       } 
+       return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions('current');
+        } catch (error) {
+            console.log(`logout error: ${error}`);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
