b280dca539d35cd83989c73369a9d9a773ee7a71
Kush Sharma <kush.73888@gmail.com>
2024-02-04 11:46:19 +0530

    React Hook Form

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,17 @@ import Footer from './Footer/Footer'
 import LogoutBtn from './Header/LogoutBtn'
 import Container from './Container/Container'
 import Logo from './Logo'
+import Button from './Button'
+import Input from './Input'
+
 
 export {
     Header,
     Footer,
     LogoutBtn,
     Container, 
-    Logo
+    Logo,
+    Button,
+    Input,
+    
 }
